9f9626be13e3d55319c5faacfdcb1d4ac24f3ba0
Nguyen Thanh Du <nguyenthdu6242@gmail.com>
2024-02-20 10:37:58 +0700

    update

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
