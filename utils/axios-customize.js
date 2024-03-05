26ba0c7011c4ee5bc20cc000798dd9a26b2e2ea4
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-06 00:41:11 +0700

    custom axios

---
+++ utils/axios-customize.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// 192.168.1.187 là IPv4 address của máy, thay đổi để test
+
+const instance = axios.create({
+  baseURL: "http://192.168.1.187:3000",
+  headers: { "X-Custom-Header": "foobar" },
+});
+
+export default instance;
