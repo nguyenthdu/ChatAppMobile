f416ca07f43d0dd4bbdd60ea96c564f3aef1242d
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-23 00:28:01 +0700

    update: update code refreshtoken

--- utils/axios-customize.js
+++ utils/axios-customize.js
@@ -1,10 +1,66 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 // 192.168.1.187 là IPv4 address của máy, thay đổi để test
 
 const instance = axios.create({
-  baseURL: "http://192.168.1.3:3000",
+  baseURL: "http://192.168.1.187:3000",
   headers: { "X-Custom-Header": "foobar" },
 });
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  async function (config) {
+    // Thêm logic để thêm token vào header của request
+    const accessToken = await AsyncStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  function (error) {
+    // Xử lý lỗi request
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Nếu response thành công, trả về response nguyên thể
+    return response;
+  },
+  async function (error) {
+    const originalRequest = error.config;
+    // Xử lý lỗi response
+    if (error.response.status === 401 && !originalRequest._retry) {
+      // Nếu mã lỗi là 401 (Unauthorized) và chưa có lần retry trước đó
+      originalRequest._retry = true;
+
+      // Lấy refresh token từ AsyncStorage
+      const refreshToken = await AsyncStorage.getItem("refreshToken");
+      if (refreshToken) {
+        // Gửi request mới để lấy access token mới từ refresh token
+        const response = await instance.post("/user/refresh-token", {
+          refreshToken,
+        });
+        const newAccessToken = response.data.accessToken;
+
+        // Lưu access token mới vào AsyncStorage
+        await AsyncStorage.setItem("accessToken", newAccessToken);
+
+        // Thực hiện lại request ban đầu với access token mới
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return instance(originalRequest);
+      } else {
+        // Nếu không có refresh token, chuyển hướng đến trang đăng nhập
+        // navigation.navigate("Login");
+        return Promise.reject(error);
+      }
+    }
+    // Trả về lỗi nếu không thực hiện được
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
