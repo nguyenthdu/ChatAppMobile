5a0c912b9b55d3b75a9721b2266455cdd87be7a7
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-03-23 15:36:51 +0700

    update: update api call backend

--- services/api.js
+++ services/api.js
@@ -2,33 +2,27 @@
 import axios from "../utils/axios-customize";
 
 // author
-export const callRegister = (
-  username,
-  password,
-  phone,
-  fullName,
-  age,
-  avatar,
-  is_admin
-) => {
-  return axios.post(`/auth/signUp`, {
-    username,
-    password,
-    phone,
-    fullName,
-    age,
-    avatar,
-    is_admin,
-  });
-};
+export const AuthAPI = {
+  register: (username, password, phone, fullName, age, avatar, is_admin) => {
+    return axios.post(`/auth/signUp`, {
+      username,
+      password,
+      phone,
+      fullName,
+      age,
+      avatar,
+      is_admin,
+    });
+  },
 
-export const callLogin = (username, password) => {
-  return axios.post("/auth/signIn", {
-    username,
-    password,
-  });
-};
+  login: (username, password) => {
+    return axios.post("/auth/signIn", {
+      username,
+      password,
+    });
+  },
 
-export const callListUserMessage = () => {
-  return axios.get("/user/users-sidebar");
+  getListUserMessage: () => {
+    return axios.get("/user/users-sidebar");
+  },
 };
