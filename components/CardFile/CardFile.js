1c88d20147a24dd3c237d4d58a13173f57e8b091
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-04-07 19:58:36 +0700

    hiênj theo loại file

--- components/CardFile/CardFile.js
+++ components/CardFile/CardFile.js
@@ -1,10 +1,46 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+const getFileIcon = (fileName) => {
+  const iconMapping = {
+    txt: "description",
+    pdf: "picture-as-pdf",
+    doc: "description",
+    docx: "description",
+    xls: "insert-drive-file",
+    xlsx: "insert-drive-file",
+    ppt: "slideshow",
+    pptx: "slideshow",
+  };
+
+  const fileExtension = fileName.split(".").pop().toLowerCase();
+
+  const icon = iconMapping[fileExtension] || "description";
+
+  return icon;
+};
+
 const CardFile = ({ fileName }) => {
+  let displayFileName = fileName; // Tên file mặc định
+
+  // Kiểm tra xem fileName có phải là một URL không
+  if (fileName.startsWith("http")) {
+    // Nếu là một URL, lấy tên file từ đường dẫn
+    try {
+      const url = new URL(fileName);
+      displayFileName = url.pathname.split("_").pop(); // Lấy phần cuối cùng của đường dẫn (tên file)
+    } catch (error) {
+      console.error("Invalid URL:", error);
+    }
+  }
+
+  const fileIcon = getFileIcon(displayFileName);
+
   return (
     <View style={styles.card}>
-      <Text style={styles.fileName}>{fileName}</Text>
+      <MaterialIcons name={fileIcon} size={24} color="black" />
+      <Text style={styles.fileName}>{displayFileName}</Text>
     </View>
   );
 };
@@ -13,6 +49,8 @@ export default CardFile;
 
 const styles = StyleSheet.create({
   card: {
+    flexDirection: "row",
+    alignItems: "center",
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 10,
@@ -27,7 +65,9 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   fileName: {
+    maxWidth: 300,
     fontSize: 16,
     fontWeight: "bold",
+    marginLeft: 10,
   },
 });
