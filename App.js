40c4528c3e1b75282b8ffa605a0f99dbdb0705f6
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-04-09 18:20:26 +0700

    update: use context api

--- App.js
+++ App.js
@@ -4,16 +4,19 @@ import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { AppProvider } from "./hooks/AppProvider";
 import StackNavigator from "./navigator/StackNavigator";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor="white" barStyle="dark-content" />
-      <StackNavigator />
-    </NavigationContainer>
+    <AppProvider>
+      <NavigationContainer>
+        <StatusBar backgroundColor="white" barStyle="dark-content" />
+        <StackNavigator />
+      </NavigationContainer>
+    </AppProvider>
   );
 }
 
---
+++ hooks/AppProvider.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+
+  return (
+    <AppContext.Provider value={{ messages, setMessages }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useChatContext = () => useContext(AppProvider);
