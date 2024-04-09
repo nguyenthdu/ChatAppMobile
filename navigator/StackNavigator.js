a2a4f2411f19fe2a12a26ba8d3aea612dbf3a52d
Nguyễn Xuân An <117873016+NguyenXuanAn5161@users.noreply.github.com>
2024-04-09 18:12:50 +0700

    update: custom laị file ap, tách nhỏ component ra

---
+++ navigator/StackNavigator.js
@@ -0,0 +1,111 @@
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+
+import AddFriend from "../screens/add_friend";
+import Chat from "../screens/chat";
+import CreateGroup from "../screens/create_group";
+import ListFriendsRequest from "../screens/list_friends_request";
+import LoginScreen from "../screens/login";
+import Logo from "../screens/logo";
+import RegisterScreen from "../screens/register";
+import RegisterPassword from "../screens/register_password";
+import StartScreen from "../screens/start";
+import UpdateProfile from "../screens/update_profile";
+
+import Contact from "../screens/contact";
+import Explore from "../screens/explore";
+import HomeScreen from "../screens/home";
+import Profile from "../screens/profile";
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Logo" component={Logo} />
+      <Stack.Screen name="Start" component={StartScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="RegisterPassword" component={RegisterPassword} />
+      <Stack.Screen name="Home" component={Root} />
+      <Stack.Screen name="Chat" component={Chat} />
+      <Stack.Screen name="AddFriend" component={AddFriend} />
+
+      <Stack.Screen name="ListFriendsRequest" component={ListFriendsRequest} />
+      <Stack.Screen name="CreateGroup" component={CreateGroup} />
+      <Stack.Screen name="UpdateProfile" component={UpdateProfile} />
+    </Stack.Navigator>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+function Root() {
+  return (
+    <Tab.Navigator
+      headerShown={false}
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        tabBarActiveTintColor: "#2f95dc",
+        tabBarInactiveTintColor: "gray",
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          paddingBottom: 3,
+          paddingTop: 3,
+          height: 60,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Tin nhắn",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="chatbubble-ellipses-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Contact"
+        component={Contact}
+        options={{
+          tabBarLabel: "Danh bạ",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="call-outline" color={color} size={size} />
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Explore"
+        component={Explore}
+        options={{
+          tabBarLabel: "Khám phá",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="apps-sharp" color={color} size={size} />
+          ),
+        }}
+      ></Tab.Screen>
+
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Cá nhân",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      ></Tab.Screen>
+    </Tab.Navigator>
+  );
+}
+
+export default StackNavigator;
